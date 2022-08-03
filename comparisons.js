562a16e881431af5a1c427cdf770cab55fe215b9
James Wasmer <jameswasmer@gmail.com>
2022-08-03 13:18:34 -0400

    Complete logic tests in parts 1, 2, and 3

--- comparisons.js
+++ comparisons.js
@@ -21,24 +21,31 @@ console.log("Is numberTeachers greater than numberStudents?", numberTeachers > n
 // this should log: "Is numberTeachers greater than numberStudents?" false
 
 // YOU DO: log the result of the comparison: is numberTeachers less than numberStudents?
+console.log(`Is numberTeachers less than numberStudents?`, numberTeachers < numberStudents);
 // this should log: true
 
 // YOU DO: log the result of the comparison: is numberTeachers strictly equal to stringTeachers?
+console.log(`Is numberTeachers strictly equal to stringTeachers?`, numberTeachers === stringTeachers);
 // this should log: false
 
 // YOU DO: log the result of the comparison: is numberTeachers not equal to numberStudents?
+console.log(`Is numberTeachers not equal to numberStudents?`, numberStudents !== numberStudents);
 // this should log: true
 
 // YOU DO: log the result of the comparison: is numberStudents greater than or equal to 20?
+console.log(`Is numberStudents greater than or equal to 20?`, numberStudents >= 20);
 // this should log: true
 
 // YOU DO: log the result of the comparison: is numberStudents greater than or equal to 21?
+console.log(`Is numberStudents greater than or equal to 21?`, numberStudents >= 21);
 // this should log: false
 
 // YOU DO: log the result of the comparison: is numberStudents less than or equal to 20?
+console.log(`Is numberStudents less than or equal to 20?`, numberStudents <= 20);
 // this should log: true
 
 // YOU DO: log the result of the comparison: is numberStudents less than or equal to 21?
+console.log(`Is numberStudents less than or equal to 21?`, numberStudents <= 21);
 // this should log: true
 
 
@@ -52,21 +59,30 @@ console.log("Is numberTeachers greater than numberStudents?", numberTeachers > n
 // Make sure YOU can explain it that way!
 
 console.log(4 < 9);
-// YOU DO: Explain.
+// This code compares whether or not the first value, 4, is less than the second value, 9.
+// If it is, it returns a boolean result of true. If it isn't, it returns false.
+// This code will return true, which the console will output.
 
 var books = 3;
 console.log(4 < books);
-// YOU DO: Explain.
+// This code assigns the number value 3 to the variable "books".
+// This code then compares whether the number 4 is less than the number value assigned to "books".
+// This code will return false, which the console will output.
 
 var friends = 6;
 var siblings = 2;
 console.log(friends > siblings);
-// YOU DO: Explain.
+// This code assigns the numbers 6 and 2 to the variables "friends" and "siblings".
+// It then compares whether the value assigned to friends is greater than the value assigned to siblings.
+// It will return true, and the console will output that.
 
 var attendees = 9;
 var meals = 8;
 console.log(attendees != meals);
-// YOU DO: Explain.
+// The values 9 and 8 are assigned to the variables "attendees" and "meals".
+// The two values are then compared, the code checking whether they are unequal.
+// If they are unequal, the console outputs a result of true. If they are equal, it outputs false.
+// The values are unequal, so the console will output true.
 
 
 // #-------------------
@@ -89,15 +105,16 @@ var age = 1;
 
 // YOU DO:
 // Determine if the dog loves to play and loves treats
-
+console.log(lovesToPlay && lovesTreats);
 
 // Determine if the dog loves to play and loves the dog park
-
+console.log(lovesToPlay && lovesDogPark);
 
 // Determine if the dog loves to play or loves the dog park
-
+console.log(lovesToPlay || lovesDogPark);
 
 // Determine if the dog loves to play and is a puppy
+console.log(lovesToPlay && age <= 1);
 
 // What did your final line of code evaluate to? Why do you think that is? Explain.
-// ANSWER:
+// ANSWER: My final line evaluated to true because I defined "puppy" as less than or equal to 1 year of age.
